ef01176cf30dcfe1a55baf595168a2d3713578ae
Sawyer Cherry <sawyercherry@macbook-pro-78.local>
2018-09-11 07:59:42 -0500

    Initial Commit

---
+++ The Real Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  The Real Task Manager
+//
+//  Created by Sawyer Cherry on 9/11/18.
+//  Copyright © 2018 Sawyer Cherry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
